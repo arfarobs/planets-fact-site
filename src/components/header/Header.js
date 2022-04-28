24bd805aaa427b44573132ea88a2edc0e9de4ebe
arfarobs <arthurroberts@mail.com>
2022-04-28 19:53:27 +0800

    Open button menu for mobile styled.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,7 @@
 import { Link } from "react-router-dom";
+import './Header.css';
+import chevron from '../../assets/images/icon-chevron.svg';
+import hamburger from'../../assets/images/icon-hamburger.svg';
 
 export const Header = () => {
   return (
@@ -6,16 +9,67 @@ export const Header = () => {
       <Link className="link" to="/">
         <h1>The Planets</h1>
       </Link>
-      <nav>
+      <button id="planet-menu-btn">
+        <img src={hamburger} alt="Menu" />
+      </button>
+      <nav className="planet-nav">
         <ul>
-          <li><Link className="nav-link link" to="/">Mercury</Link></li>
-          <li><Link className="nav-link link" to="/venus">Venus</Link></li>
-          <li><Link className="nav-link link" to="/earth">Earth</Link></li>
-          <li><Link className="nav-link link" to="/mars">Mars</Link></li>
-          <li><Link className="nav-link link" to="/jupiter">Jupiter</Link></li>
-          <li><Link className="nav-link link" to="/saturn">Saturn</Link></li>
-          <li><Link className="nav-link link" to="/uranus">Uranus</Link></li>
-          <li><Link className="nav-link link" to="/neptune">Neptune</Link></li>
+          <li>
+            <Link className="planet-link link" to="/">
+              <span className="planet-link-icon" id="mercury-icon"></span>
+              Mercury
+              <img className="chevron" src={chevron} aria-hidden="true" alt=""/>
+            </Link>
+          </li>
+          <li>
+            <Link className="planet-link link" to="/venus">
+              <span className="planet-link-icon" id="venus-icon"></span>
+              Venus
+              <img className="chevron" src={chevron} aria-hidden="true" alt=""/>
+            </Link>
+          </li>
+          <li>
+            <Link className="planet-link link" to="/earth">
+              <span className="planet-link-icon" id="earth-icon"></span>
+              Earth
+              <img className="chevron" src={chevron} aria-hidden="true" alt=""/>
+            </Link>
+          </li>
+          <li>
+            <Link className="planet-link link" to="/mars">
+              <span className="planet-link-icon" id="mars-icon"></span>
+              Mars
+              <img className="chevron" src={chevron} aria-hidden="true" alt=""/>
+            </Link>
+          </li>
+          <li>
+            <Link className="planet-link link" to="/jupiter">
+              <span className="planet-link-icon" id="jupiter-icon"></span>
+              Jupiter
+              <img className="chevron" src={chevron} aria-hidden="true" alt=""/>
+            </Link>
+          </li>
+          <li>
+            <Link className="planet-link link" to="/saturn">
+              <span className="planet-link-icon" id="saturn-icon"></span>
+              Saturn
+              <img className="chevron" src={chevron} aria-hidden="true" alt=""/>
+            </Link>
+          </li>
+          <li>
+            <Link className="planet-link link" to="/uranus">
+              <span className="planet-link-icon" id="uranus-icon"></span>
+              Uranus
+              <img className="chevron" src={chevron} aria-hidden="true" alt=""/>
+            </Link>
+          </li>
+          <li>
+            <Link className="planet-link link" to="/neptune">
+              <span className="planet-link-icon" id="neptune-icon"></span>
+              Neptune
+              <img className="chevron" src={chevron} aria-hidden="true" alt=""/>
+            </Link>
+          </li>
         </ul>
       </nav>
     </header>
