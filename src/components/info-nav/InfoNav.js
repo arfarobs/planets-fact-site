1e56f44267af2f8df321135d093619886c87ed6d
arfarobs <arthurroberts@mail.com>
2022-05-11 20:43:43 +0800

    fixed info nav button issue

--- src/components/info-nav/InfoNav.js
+++ src/components/info-nav/InfoNav.js
@@ -1,29 +1,30 @@
 import './InfoNav.css';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { changeParagraph } from '../info/infoSlice';
+import { useEffect } from 'react';
 
 export const InfoNav = () => {
+  const currentSection = useSelector((state) => state.info.currentSection);
   const dispatch = useDispatch();
 
-  const changeActive = (e) => {
-    const infoNavItems = document.getElementsByClassName('info-nav-item');
-    console.log(infoNavItems);
-    for (let i = 0; i < infoNavItems.length; i++) {
-      infoNavItems[i].classList.remove('selected-info-nav-item');
+  useEffect(() => {
+    const infoNavButtons = document.getElementsByClassName('info-nav-button');
+    for (let i = 0; i < infoNavButtons.length; i++) {
+      infoNavButtons[i].classList.remove('selected-info-nav-button');
+      if (infoNavButtons[i].getAttribute('data-title') === currentSection) {
+        infoNavButtons[i].classList.add('selected-info-nav-button');
+      }
     }
-    e.target.parentElement.parentElement.classList.add('selected-info-nav-item');
-  }
+  }, [currentSection]);
 
   const navTitles = ['overview', 'structure', 'surface'];
   const navListItems = navTitles.map((title, index) => {
     return (
-      <li className={index === 0 ? 'selected-info-nav-item info-nav-item' : 'info-nav-item'} key={index}>
+      <li key={index}>
         <button 
-          className="info-nav-button" 
-          onClick={(e) => {
-            dispatch(changeParagraph(title));
-            changeActive(e);
-          }} >
+          data-title={title}
+          className={index === 0 ? 'selected-info-nav-button info-nav-button' : 'info-nav-button'}
+          onClick={() => dispatch(changeParagraph(title))} >
           <span className="info-nav-number">0{index + 1}</span>
           {index === 1 && <span className="tablet-title">internal&nbsp;</span>}
           <h3>{title}</h3>
