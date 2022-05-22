a23db49f3b67de3cad74ef350c9242a878fbe01f
arfarobs <arthurroberts@mail.com>
2022-05-22 10:29:20 +0800

    info page change animations added

--- src/components/img/Img.js
+++ src/components/img/Img.js
@@ -14,13 +14,9 @@ export const Img = () => {
   const imgVariants = {
     initial: {
       scale: 0,
-      x: '-70vw',
-      y: '-70vh'
     },
     animate: {
       scale: 1,
-      x: 0,
-      y: 0,
       transition: {
         duration: 1,
         ease: 'easeOut',
@@ -28,12 +24,10 @@ export const Img = () => {
       }
     },
     exit: {
-      x: '-70vw',
-      y: '-70vh',
       scale: 0,
       opacity: 0,
       transition: {
-        duration: 1,
+        duration: 0.5,
         ease: 'easeIn'
       }
     }
--- src/components/info/Info.js
+++ src/components/info/Info.js
@@ -1,27 +1,47 @@
 import './Info.css';
 import { useSelector } from 'react-redux';
+import { motion } from 'framer-motion';
 
 export const Info = () => {
   const paragraph = useSelector((state) => state.info.paragraph);
   const heading = useSelector((state) => state.info.currentPage);
   const wikiSrc = useSelector((state) => state.info.wikiSrc);
 
+  const variants = {
+    visible: (i) => ({
+      opacity: i === 3 ? 0.5 : 1,
+      transition: {
+        delay: i * 0.5,
+      },
+    }),
+    hidden: { opacity: 0 },
+    exit: {
+      opacity: 0
+    }
+  }
+  
+
   return (
-    <div className="info-container">
-      <h2>{heading}</h2>
-      <p className='info-p'>
+    <motion.div className="info-container" variants={variants} animate='visible' initial='hidden' exit='exit'>
+      <motion.h2 variants={variants} custom={1}>{heading}</motion.h2>
+      <motion.p className='info-p' variants={variants} custom={2}>
         {paragraph}
-      </p>
-      <p className="source">Source :&nbsp;
-        <a 
+      </motion.p>
+      <motion.p className="source" 
+        variants={variants} 
+        custom={3}
+      >Source :&nbsp;
+        <motion.a 
+          variants={variants}
+          custom={4}
           href={wikiSrc} 
           id="wiki" 
           target="_blank" 
           rel="noreferrer"
         >
           Wikipedia
-        </a>
-      </p>
-    </div>
+        </motion.a>
+      </motion.p>
+    </motion.div>
   )
 }
